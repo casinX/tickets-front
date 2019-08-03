01b535b73dcaa86e423fb379202bd21913d6fcc3
Anton Chashchin <anton.casin@gmail.com>
2019-08-03 23:22:31 +0300

    udpate polyfilling

--- webpack/rules/JSXRule/index.js
+++ webpack/rules/JSXRule/index.js
@@ -4,14 +4,16 @@ export default () => ({
   loader: 'babel-loader',
   query: {
     presets: [
-      [ '@babel/preset-env', { 'targets': { 'browsers': 'last 2 versions' } } ],
-      '@babel/preset-react'
+      [ '@babel/preset-env', {
+        useBuiltIns: 'usage',
+        corejs: 3,
+      } ],
+      '@babel/preset-react',
     ],
     plugins: [
-      ['@babel/plugin-transform-runtime', { 'corejs': 3, 'proposal': true, }],
-      ['@babel/plugin-proposal-decorators', { 'legacy': true }],
-      ['@babel/plugin-proposal-class-properties', { 'loose' : true }],
-      '@babel/plugin-proposal-object-rest-spread',
+      ['@babel/plugin-transform-runtime'],
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      ['@babel/plugin-proposal-class-properties', { loose : true }],
     ]
   }
 })
